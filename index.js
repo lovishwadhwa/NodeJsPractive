f77b9859c827e514dad606802168b72ed46a3296
lovishwadhwa <51968938+lovishwadhwa@users.noreply.github.com>
2020-06-22 23:40:17 +0530

    Intial React App and NodeJs Server with Express for serving the react app (#1)
    First Step of setting up NodeJS Server with Express and React App

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "/antstack", "build")));
+app.use(express.static("public"));
+
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000!')
+});
