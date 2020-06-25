f7f6ccd97697cda50531937ad16143ba5c1dbb53
lovishwadhwa <51968938+lovishwadhwa@users.noreply.github.com>
2020-06-26 02:09:17 +0530

    Api Configured with React App for Meta Tag fetching with Build Update (#3)
    Meta Tag feature has been embedded with API support and Build Update of React App

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
+//const cors = require('cors');
 const app = express();
 const path = require("path");
 const apis = require("./apis/routes");
 
+//use cors to allow cross origin resource sharing
+/*app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);*/
+
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "/antstack", "build")));
-app.use('', apis);
+app.use('/apis', apis);
 
 
 app.listen(8000, () => {
