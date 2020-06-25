d1eb7287a32d447f291834887691f4c086bd20c1
Lovish <lovish.wadhwa11@live.com>
2020-06-26 02:07:53 +0530

    Api Configured with React App for Meta Tag fetching with Build Update

--- antstack/src/components/QueryCards.js
+++ antstack/src/components/QueryCards.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, Button, Row, Col, Form, Badge, Container} from 'react-bootstrap';
+import {Card, Button, Row, Col, Form, Badge, Container, Toast} from 'react-bootstrap';
 
 import axios from 'axios';
 
@@ -10,18 +10,44 @@ class QueryCards extends React.Component {
             response  : "You can see the last response here",
             isLoading1 : false,
             isLoading2 : false,
+            url        : "",
+            meta       : "",
         };
-        this.findMetaTag = this.findMetaTag.bind(this);
     }
-    async findMetaTag () {
-        let response = "";
-        this.setState({isLoading1: true});
-        await axios.post('/apis/hello', {message: "hello"}).then( (res)=>{
-            response = res.data.message;
-            console.log(response);
-            console.log(res);
-        });
-        this.setState({isLoading1: false, response: response });
+    findMetaTag = async () => {
+        if(this.state.url.trim()!="" && this.state.meta.trim()!=""){
+            let response = "";
+            this.setState({isLoading1: true, response: "..."});
+            await axios.post('/apis/findMeta', {url: this.state.url, meta: this.state.meta}).then( (res)=>{
+                response = res.data.message;
+                console.log(response);
+                console.log(res);
+            });
+            this.setState({isLoading1: false, response: response });
+        }
+        else{
+            this.setState({response: "Please provide both the inputs before querying"})
+        }
+    }
+
+    validURL(myURL) {
+        var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ //port
+        '(\\?[;&amp;a-z\\d%_.~+=-]*)?'+ // query string
+        '(\\#[-a-z\\d_]*)?$','i');
+        return pattern.test(myURL);
+     }
+
+    handleInputChange = (event) => {
+        if(event.target.name=="url"){
+            if(!(this.validURL(event.target.value))){
+                this.setState({response: "Please provide a valid url as input", url: ""});
+                return;
+            }
+        }
+        this.setState({[event.target.name]: event.target.value});
     }
     render () {
 
@@ -35,14 +61,14 @@ class QueryCards extends React.Component {
                             <Card>
                                 <Card.Body>
                                     <Card.Title>Query For Meta Tag Here</Card.Title>
-                                        <Form>
+                                        <Form >
                                             <Form.Group controlId="formGroupURL">
                                                 <Form.Label>URL</Form.Label>
-                                                <Form.Control type="url" placeholder="Enter the url for lookup" />
+                                                <Form.Control as='input' type="url" value={this.state.url} name="url" onChange={this.handleInputChange} placeholder="Enter the url for lookup" />
                                             </Form.Group>
                                             <Form.Group controlId="formGroupMETA">
                                                 <Form.Label>Meta Tag</Form.Label>
-                                                <Form.Control type="text" placeholder="Enter the meta tag you want to search for" />
+                                                <Form.Control as='input' type="text" value={this.state.meta} name="meta" onChange={this.handleInputChange} placeholder="Enter the meta tag you want to search for" />
                                             </Form.Group>
                                         </Form>
                                     <Button variant="primary" disabled={this.state.isLoading}  onClick={!this.state.isLoading1 ? this.findMetaTag : null}>{ this.state.isLoading1 ? 'Processing...': 'Find Response'}</Button>
@@ -68,9 +94,13 @@ class QueryCards extends React.Component {
                             </Card>
                         </Col>
                     </Row>
-                    <div className="text-center h3">
-                        <Badge variant="success">{this.state.response}</Badge>
-                    </div>
+                    <center>
+                    <Card style={{width: '30rem'}} className="bg-success p-2">
+                        <Card.Text className="h5">
+                            {this.state.response}
+                        </Card.Text>
+                    </Card>
+                    </center>
                 </Container>
                 );
     }
--- apis/routes.js
+++ apis/routes.js
@@ -1,9 +1,42 @@
 const express = require('express');
-const router  = express.Router(); 
+const router = express.Router();
 
-router.route("/apis/hello").post((req, res) => {
-    console.log(req);
-    res.send({message: "hello"});
+const urlMetadata = require('url-metadata');
+
+router.route("/findMeta").post(async (req, res) => {
+    try {
+        let reqBody = req.body;
+        console.log(reqBody);
+        if (!("url" in req.body || "meta" in req.body))
+            res.send({
+                message: "Query's format is not correct"
+            });
+        else {
+            let url = req.body.url;
+            let meta = req.body.meta;
+            try {
+                let metaData = await urlMetadata(url);
+                console.log(metaData);
+                if (meta in metaData) {
+                    res.send({
+                        message: metaData[meta]
+                    });
+                } else {
+                    res.send({
+                        message: "This meta tag is not present in the url"
+                    });
+                }
+            } catch (err) {
+                res.send({
+                    message: "There is an error in fetching meta tags for the given url"
+                });
+            }
+        }
+    } catch (err) {
+        res.send({
+            message: "There is an error in the user inputs"
+        })
+    }
 })
 
 
