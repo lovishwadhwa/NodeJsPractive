36256ac9233210ceb18a7d67cc7172184e22868e
Lovish <lovish.wadhwa11@live.com>
2020-06-26 17:29:28 +0530

    BugFix for port number for Heroku

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ app.use(express.static(path.join(__dirname, "/antstack", "build")));
 app.use('/apis', apis);
 
 
-app.listen((process.env.PORT||5000)), () => {
+app.listen((process.env.PORT||8000), () => {
   console.log('Example app listening on port 8000!')
 });
